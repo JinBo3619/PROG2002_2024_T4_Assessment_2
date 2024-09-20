06d12f84966c45be14685b971725ca8e4822f3b6
JinBo3619 <2088917684@qq.com>
2024-09-20 11:01:56 +0800

    [+] Add a database connection file

---
+++ api/db/crowdfunding_db.js
@@ -0,0 +1,21 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  database : 'crowdfunding_db'
+});
+ 
+connection.connect((err) => {
+  if (err) {
+    console.error('数据库连接失败: ' + err.stack);
+    return;
+  }
+  console.log('成功连接到数据库，连接ID: ' + connection.threadId);
+});
+ 
+// connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+//   if (error) throw error;
+//   console.log('The solution is: ', results[0].solution);
+// });
+module.exports = connection;
