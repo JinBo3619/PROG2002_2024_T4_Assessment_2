ef6f66f022e385daef61ae4b4ab05646481a48ee
JinBo3619 <2088917684@qq.com>
2024-09-20 17:32:28 +0800

    [*] Modify the style of the crowdfunding information card on the homepage

--- api/routes/api.js
+++ api/routes/api.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const db = require('../db/crowdfunding_db');
 
 router.get('/fundraisers', (req, res) => {
-  db.query('SELECT a.*,b.`NAME` FROM FUNDRAISER a LEFT JOIN CATEGORY b ON a.CATEGORY_ID = b.CATEGORY_ID WHERE a.ACTIVE = 1', (err, results) => {
+  db.query('SELECT a.*,b.`NAME`AS CATEGORY_NAME FROM FUNDRAISER a LEFT JOIN CATEGORY b ON a.CATEGORY_ID = b.CATEGORY_ID WHERE a.ACTIVE = 1', (err, results) => {
     if (err) return res.status(500).json(err);
     res.json(results);
   });
